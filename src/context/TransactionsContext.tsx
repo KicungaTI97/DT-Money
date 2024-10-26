99a2849b0588eec41e564b348e1310285d626bf5
Joao Kicunga <kicungati@gmail.com>
2024-10-26 09:58:39 +0100

    Performance - Aplicando o useCallback

--- src/context/TransactionsContext.tsx
+++ src/context/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useState, useCallback } from "react";
 import { api } from "../lib/axios";
 import { createContext } from "use-context-selector";
 
@@ -37,40 +37,45 @@ export function TransactionsProvider({children}:TransactionsProviderProps){
     const [transactions, setTransactions] = useState<Transaction[]>([])
 
     // Função para buscar transações do servidor. Pode aceitar uma query opcional para filtrar resultados
-    async function fetchTransactions(query?: string){
+    const fetchTransactions = useCallback(
+        async (query?: string) =>{
    
-        try {
-             // Faz uma requisição GET para buscar transações e ordena os resultados por data (mais recentes primeiro)
-            const response = await api.get('transactions',{
-                params: {
-                    q: query, // Filtrar resultados por query se for fornecida
-                    _sort: 'createdAt',
-                    _order: 'desc',
-                 
-                },
-            })
-            setTransactions(response.data); // Atualiza o estado com as transações obtidas
-        } catch (error) {
-            console.log('Error fetching transactions:',error,)
-     }
-}
-
+            try {
+                 // Faz uma requisição GET para buscar transações e ordena os resultados por data (mais recentes primeiro)
+                const response = await api.get('transactions',{
+                    params: {
+                        q: query, // Filtrar resultados por query se for fornecida
+                        _sort: 'createdAt',
+                        _order: 'desc',
+                     
+                    },
+                })
+                setTransactions(response.data); // Atualiza o estado com as transações obtidas
+            } catch (error) {
+                console.log('Error fetching transactions:',error,)
+         }
+    },[]
+    
+    )
 // Função para criar uma nova transação e enviar para o servidor
-    async function createTransaction(data: CreateTransactionInput){
-        const {description, price, category, type} = data;
-
-        // Faz uma requisição POST para criar uma nova transação e adiciona-a à lista de transações
-        const response = await api.post('transactions', {
-            description,
-            price,
-            category,
-            type,
-            createdAt: new Date(), // Define a data de criação como o momento atual
-        })
-
-         // Adiciona a nova transação ao estado (no início da lista)
-        setTransactions(state => [response.data, ...state])
-    }
+    const createTransaction = useCallback(
+        async (data: CreateTransactionInput) =>{
+            const {description, price, category, type} = data;
+    
+            // Faz uma requisição POST para criar uma nova transação e adiciona-a à lista de transações
+            const response = await api.post('transactions', {
+                description,
+                price,
+                category,
+                type,
+                createdAt: new Date(), // Define a data de criação como o momento atual
+            },[],
+        )
+    
+             // Adiciona a nova transação ao estado (no início da lista)
+            setTransactions(state => [response.data, ...state])
+        }
+    )
 
     // useEffect para buscar transações automaticamente quando o componente for montado
     useEffect(()=>{
